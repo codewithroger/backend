5fd6008ec6d9757915f2cf204c8c427477f35523
Yogesh Hitnalli <117502443+codewithroger@users.noreply.github.com>
2025-06-23 17:00:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,9 +10,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.error('MongoDB connection error:', err));
+mongoose.connect(process.env.MONGO_URI, {
+});
 
 app.use('/api/auth', authRoutes);
 app.use('/api/products', productRoutes);
